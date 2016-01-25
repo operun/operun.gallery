1cf6e7c71f9040cf6b4700e61794b75e084b4dec
Jesse Stippel <SkyeStippel@Gmail.com>
2016-01-25 23:07:34 +0100

    gallery responsive

--- src/operun/gallery/browser/static/gallery.js
+++ src/operun/gallery/browser/static/gallery.js
@@ -6,10 +6,26 @@ if (window.jQuery) {
 
 require([
   'jquery',
+  'hoverintent',
   'lightbox',
 ], function($) {
   'use strict';
 
   // Custom variables
-
+  $(document).ready(function() {
+    $('.gallery-image').each(function() {
+      $(this).delay(Math.floor(Math.random() * 1000)).animate({
+        opacity: 1
+      }, 250);
+    });
+    $(".gradient-overlay").css({
+      'height': ($(".gallery-image").height() + 'px')
+    });
+    $('.gallery-image').hoverIntent(function() {
+        $(this).find('.title-container').fadeIn(100);
+      },
+      function() {
+        $(".title-container").delay(100).fadeOut(100);
+      });
+  });
 });
